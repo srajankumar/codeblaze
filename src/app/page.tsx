cd680f8b64f2c59c5ea2056fd85c5f53e732e949
srajan <kumarsrajan02@gmail.com>
2023-11-10 10:24:23 +0530

    added scroll indication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,14 +43,34 @@ export default function Home() {
           </div>
         </div>
 
+        <div className="absolute z-40 float1 bottom-0 py-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="currentColor"
+              d="M104.704 338.752a64 64 0 0 1 90.496 0l316.8 316.8l316.8-316.8a64 64 0 0 1 90.496 90.496L557.248 791.296a64 64 0 0 1-90.496 0L104.704 429.248a64 64 0 0 1 0-90.496z"
+            />
+          </svg>
+        </div>
+
         {/* <RegisterButton /> */}
-        <img
+        <Image
+          width={700}
+          height={500}
+          priority
           className="absolute opacity-50 right-0 top-full -z-10"
           src="/assets/background/blur1.png"
           alt=""
         />
-        <img
-          className="absolute opacity-80 top-full pt-[40rem] left-0 -z-10"
+        <Image
+          width={700}
+          height={500}
+          priority
+          className="absolute opacity-80 top-full pt-[42rem] left-0 -z-10"
           src="/assets/background/blur2.png"
           alt=""
         />
