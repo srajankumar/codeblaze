261ed956ac8f9b4d2735f0f1f6434f9a94b52ed3
srajan <kumarsrajan02@gmail.com>
2023-11-17 15:13:33 +0530

    hackathon registrations open

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,8 +6,7 @@ export const siteConfig = {
   scem: "https://sahyadri.edu.in/",
   description:
     "Codeblaze is an extraordinary tech extravaganza by Sahyadri Open Source Community (SOSC), is set to be a thrilling event spanning technical and non-technical domains. It aims to empower participants with knowledge and skills while fostering curiosity and innovation. With a variety of tech-focused events, it offers opportunities for growth and self-discovery. Join the 20-hour on-site Hackathon to turn your innovative ideas into reality, with themes suitable for all skill levels.",
-  register: "",
-  // register: "https://sankalp.sosc.org.in/",
+  register: "https://sankalp.sosc.org.in/",
   url: "https://codeblaze.sosc.org.in",
   ogImage: "https://codeblaze.sosc.org.in/og.png",
   links: {
